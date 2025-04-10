063ae892f37fa7b11c83637f53893d8451eeac27
Q∀Z <n3@nitbit.dev>
2025-04-11 01:39:56 +0800

    workflows part 4

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/notesheet'
-		}
-	}
-};
-
-export default config;
+}
